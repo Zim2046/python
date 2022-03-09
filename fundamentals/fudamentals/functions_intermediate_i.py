ff79e98e11c21eab0c8e3d615d6b1333e02805e6
Zim2046 <99115132+Zim2046@users.noreply.github.com>
2022-03-09 08:37:51 -0800

    complete

--- fundamentals/fudamentals/functions_intermediate_i.py
+++ fundamentals/fudamentals/functions_intermediate_i.py
@@ -1,4 +1,5 @@
-# ''' #1) Update Values in Dictionaries and Lists '''
+''' #1) Update Values in Dictionaries and Lists -------------------------------------------'''
+
 # x = [[5, 2, 3], [10, 8, 9]]
 # students = [
 #     {'first_name':  'Michael', 'last_name': 'Jordan'},
@@ -26,39 +27,63 @@
 # z[0]['y'] = 30
 # print(z)
 
-''' #2) Iterate Through a List of Dictionaries'''
-from array import array
-from cgi import print_arguments
+''' #2) Iterate Through a List of Dictionaries -----------------------------------------------'''
+
+# students = [
+#     {'first_name':  'Michael', 'last_name': 'Jordan'},
+#     {'first_name': 'John', 'last_name': 'Rosales'},
+#     {'first_name': 'Mark', 'last_name': 'Guillen'},
+#     {'first_name': 'KB', 'last_name': 'Tonel'}
+# ]
 
 
-students = [
-    {'first_name':  'Michael', 'last_name': 'Jordan''middle_name':},
-    {'first_name': 'John', 'last_name': 'Rosales'},
-    {'first_name': 'Mark', 'last_name': 'Guillen'},
-    {'first_name': 'KB', 'last_name': 'Tonel'}
-]
-
-'''#3 Get Values From a List of Dictionaries'''
-
-
-def iterateDictionary(some_list):
-    for index in range(len(some_list)):
-        array = []
-        for k, v in some_list[index].items():
-            a = "{}".format(k)
-            b = "{}".format(v)
-            c = '{} - {}'.format(a, b)
-            array.append(c)
-        # print(f"{array[0]}, {array[1]}")
-        print('{}, {}'.format(array[0], array[1]))
+# def iterateDictionary(some_list):
+#     for index in range(len(some_list)):
+#         array = []
+#         for k, v in some_list[index].items():
+#             c = '{} - {}'.format(k, v)
+#             array.append(c)
+#         # print(f"{array[0]}, {array[1]}")
+#         print(f'{array[0]}, {array[1]}')
 
 
-iterateDictionary(students)
+# iterateDictionary(students)
 
-# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# should output: (it's okay if each key-value pair ends up on 2 separate lines
 
 # bonus to get them to appear exactly as below!)
 # first_name - Michael, last_name - Jordan
 # first_name - John, last_name - Rosales
 # first_name - Mark, last_name - Guillen
 # first_name - KB, last_name - Tonel
+
+'''#3) Get Values From a List of Dictionaries ------------------------------------------------'''
+
+
+# def iterateDictionary2(key_name, some_list):
+#     for index in range(len(some_list)):
+#         print(some_list[index][key_name])
+
+
+# iterateDictionary2('first_name', students)
+# iterateDictionary2('last_name', students)
+
+
+''' 4) Iterate Through a Dictionary with List Values ------------------------------------------'''
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+
+def printInfo(some_dict):
+    for k, v in some_dict.items():
+        listLength_title = f'{len(v)} {k.upper()}'
+        print(listLength_title)
+        for i in v:
+            print(f'{i}')
+        print('\n')
+
+
+printInfo(dojo)
