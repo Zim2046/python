4493757568e00dc18b6ebe234f87dec1f8c028f0
Zim2046 <99115132+Zim2046@users.noreply.github.com>
2022-03-09 18:00:33 -0800

    initial commit

---
+++ fundamentals/fudamentals/function_basics/recognize_python/recognize_python.py
@@ -0,0 +1,10 @@
+# this is a single line comment
+"""
+This is
+a multiline
+comment
+"""
+'''Comments as well
+    more comments
+'''
+my_new_favorite_language = 'Python'  # variable declaration, initialize string
