55fab8ff28d224f757588ea4f0229e01a89ea223
Zim2046 <99115132+Zim2046@users.noreply.github.com>
2022-03-10 18:20:13 -0800

    initial commit

---
+++ flask/flask_fundamentals/understanding_routing/server.py
@@ -0,0 +1,68 @@
+from flask import Flask, request  # Import Flask to allow us to create our app
+# Create a new instance of the Flask class called "app"
+app = Flask(__name__)
+
+
+# The "@" decorator associates this route with the function immediately following
+@app.route('/')
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+
+# import statements, maybe some other routes
+
+# 2)
+
+
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+
+# 3)
+
+
+@app.route('/say/<string:name>')
+def hello(name):
+    return f'Hi {name}!'
+
+# 4)
+# for a route '/users/____/____', two parameters in the url get passed as username and id
+
+
+@app.route('/repeat/<int:num>/<string:name>')
+def show_user_profile(num, name):
+    return f'{name*num}'
+
+# SENSEI BONUS: Ensure that if the user types in any route other than the ones specified, they receive an error message saying "Sorry! No response. Try again."
+
+
+# @app.route('/<input>')
+# def hello_world(input):
+#     a = 'dojo'
+#     b = 'say/<string:name>'
+#     c = 'repeat/<int:num>/<string:name>'
+#     if
+#     return 'Sorry! No response. Try again.'
+
+# @app.route('/<input>')
+# def show_user_profile(input):
+#     return 'Sorry! No response. Try again.'
+# # app.run(debug=True) should be the very last statement!f
+
+
+# @app.route('/<input>')
+# def error_trailing_name(input):
+#     rp = request.path
+#     expressionOe =
+#     if rp.endswith() != '/' != '/' or input != '/repeat/<int:num>/<string:name>' or input != '/say/<string:name>'
+#     return 'Sorry! No response. Try again.'
+# # app.run(debug=True) should be the very last statement!f
+
+
+# def clear_trailing():
+#     rp = request.path
+#     if rp.endswith() != '/':
+#         return 'Sorry! No response. Try again.'
+
+
+if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
+    app.run(debug=True)    # Run the app in debug mode.
