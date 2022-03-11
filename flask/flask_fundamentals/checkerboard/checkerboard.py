9348ee8159221cb67689b96f54e27431b604d6b8
Zim2046 <99115132+Zim2046@users.noreply.github.com>
2022-03-11 09:56:56 -0800

    >6152CH46t!m

--- flask/flask_fundamentals/checkerboard/checkerboard.py
+++ flask/flask_fundamentals/checkerboard/checkerboard.py
@@ -24,4 +24,4 @@ def customColor(width, height, colorOneIn, colorTwoIn):
 
 
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
-    app.run(debug=True)    # Run the app in debug mode.
+    app.run(debug=True)
