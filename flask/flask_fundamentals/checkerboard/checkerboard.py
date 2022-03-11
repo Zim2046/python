e802c89ed53844c4f3caec04b66e80a162660cf1
Zim2046 <99115132+Zim2046@users.noreply.github.com>
2022-03-11 09:34:50 -0800

    >6152CH46t!m

--- flask/flask_fundamentals/checkerboard/checkerboard.py
+++ flask/flask_fundamentals/checkerboard/checkerboard.py
@@ -1,4 +1,4 @@
-from turtle import color
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -23,6 +23,5 @@ def customColor(width, height, colorOneIn, colorTwoIn):
     return render_template("checkerboard.html", colDimension=height, rowDimension=width,  colorOne=colorOneIn, colorTwo=colorTwoIn)
 
 
-# app.run(debug=True) should be the very last statement!
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
     app.run(debug=True)    # Run the app in debug mode.
