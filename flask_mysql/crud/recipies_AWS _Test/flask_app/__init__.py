c3d8d08b61616f4cc6e8c1345af44dceb16dd13b
Timothy Thomas <inn-tanner0r@icloud.com>
2022-03-29 00:33:15 -0700

    update

--- flask_mysql/crud/recipies_AWS _Test/flask_app/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from flask_bcrypt import Bcrypt #bcrypt is handled in the controllers (Models)
-from flask import Flask,flash
-import re
-
-app = Flask(__name__)
-
-#👇 We need this for session/flash
-app.secret_key = "shhhhhhhhhhh!"
